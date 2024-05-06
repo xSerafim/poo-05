c87cd3fe0cc03bcb9f9a5b8a5c8a688ae7e9119b
Guilherme Serafim Bonfim <guilhermeserafimbonfim@hotmail.com>
2024-05-05 21:02:15 -0300

    ex01

---
+++ ex01/src/Teste.java
@@ -0,0 +1,33 @@
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("Inicio do main");
+        try {
+            metodo1();
+        } catch (ArithmeticException e) {
+            System.out.println("Erro: " + e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+        System.out.println("Fim do main");
+    }
+
+    public static void metodo1() {
+        System.out.println("Inicio do método 1");
+        metodo2();
+        System.out.println("Fim do método 1");
+    }
+
+    public static void metodo2() throws ArithmeticException, ArrayIndexOutOfBoundsException {
+        System.out.println("Inicio do método 2");
+        int[] vetor = new int[10];
+        for (int i = 0; i <= 15; i++) {
+            vetor[i] = i;
+            System.out.println(i);
+            if (i == 9) {
+                int vlr = vetor[i] / vetor[0];
+                System.out.println(vlr);
+            }
+        }
+        System.out.println("Fim do método 2");
+    }
+}
